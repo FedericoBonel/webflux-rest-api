d33beb5722805c0a3ac92c075f1e25473746aaea
Federico Jorge Bonel Tozzi <Bonelfederico@gmail.com>
2022-06-24 16:51:10 +0900

    Initial commit

---
+++ src/main/java/com/federicobonel/webfluxrestapi/WebfluxRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.federicobonel.webfluxrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/federicobonel/webfluxrestapi/WebfluxRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.federicobonel.webfluxrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
